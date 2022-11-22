77b4d716dd096ba36a157a65c6ac6f3224eacf33
kosu109 <220155@jc-21.jp>
2022-11-22 15:07:47 +0900

    11/22

---
+++ kadai1/koji_add_check.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="UTF-8">
+        <title>ろくまる農園</title>
+    </head>
+    <body>
+        <?php
+
+        $koji_ID=$_POST['ID'];
+        $koji_name=$_POST['name'];
+        $koji_name2=$_POST['name2'];
+        $koji_yubin=$_POST['yubin'];
+        $koji_jusho=$_POST['jusho'];
+        ?>
+    </body>
+</html>
